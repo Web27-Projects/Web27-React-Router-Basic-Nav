cc3eaebd6cf57026ffb006091b2055ee7a7e944e
Miley Wright <dishaliekwright@yahoo.com>
2020-01-27 13:32:19 -0500

    Complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
-import {Route, Link} from 'react-router-dom';
+import {Route} from 'react-router-dom';
 
 const App = () => (
   <div>
